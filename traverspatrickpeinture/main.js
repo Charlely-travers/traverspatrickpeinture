05efb15bb13b9ee090482948af73aab7b3e1dfee
Charlely-travers <charlely11@icloud.com>
2023-02-04 18:32:18 +0100

    test envoie mail

--- traverspatrickpeinture/main.js
+++ traverspatrickpeinture/main.js
@@ -28,32 +28,37 @@ createApp({
   methods: {
     envoieEmail()
     {
-      console.log(this.formEmail);
-      this.chargement=true;
-      let data=new FormData();
-      data.append('email',this.formEmail.email);
-      data.append('nom',this.formEmail.nom);
-      data.append('prenom',this.formEmail.prenom);
-      data.append('tel',this.formEmail.tel);
-      data.append('lieu',this.formEmail.lieu);
-      data.append('message',this.formEmail.message);
-      console.log(data);
-      fetch('/traverspatrickpeinture/index.php/sendEmail',{
-        method:'post',
-        body:data
-      })
-        .then( (response) => {
-          this.showModalCommande= false;
-          this.chargement=false;
-          this.formEmail.email=null;
-          this.formEmail.nom=null;
-          this.formEmail.prenom=null;
-          this.formEmail.tel=null;
-          this.formEmail.lieu=null;
-          this.formEmail.message=null;
-          console.log(this.formEmail);
-          //window.location.href="./remerciement.html";
-      })
+      // console.log(this.formEmail);
+      // this.chargement=true;
+      // let data=new FormData();
+      // data.append('email',this.formEmail.email);
+      // data.append('nom',this.formEmail.nom);
+      // data.append('prenom',this.formEmail.prenom);
+      // data.append('tel',this.formEmail.tel);
+      // data.append('lieu',this.formEmail.lieu);
+      // data.append('message',this.formEmail.message);
+      // console.log(data);
+      // fetch('/traverspatrickpeinture/index.php/sendEmail',{
+      //   method:'post',
+      //   body:data
+      // })
+      //   .then( (response) => {
+      //     this.showModalCommande= false;
+      //     this.chargement=false;
+      //     this.formEmail.email=null;
+      //     this.formEmail.nom=null;
+      //     this.formEmail.prenom=null;
+      //     this.formEmail.tel=null;
+      //     this.formEmail.lieu=null;
+      //     this.formEmail.message=null;
+      //     console.log(this.formEmail);
+      //     //window.location.href="./remerciement.html";
+      // })
+
+      this.chargement = true;
+      this.showModalCommande= false;
+      this.chargement = false;
+      window.location.href="./remerciement.html";
     },
 },
 }).mount('#app')
